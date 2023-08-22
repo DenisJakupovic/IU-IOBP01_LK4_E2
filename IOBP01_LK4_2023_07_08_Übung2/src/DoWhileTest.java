b1ff792e626164671d83f220eecc9d48e8e5dfff
DenisJakupovic <denis.jakupovic@gmx.ch>
2023-08-22 19:30:56 +0200

    IU-IOBP01_LK4_E2

---
+++ IOBP01_LK4_2023_07_08_Übung2/src/DoWhileTest.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class DoWhileTest {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner (System.in);
+		int zahl = 0;
+		int summe = 0;
+		do
+		{
+			System.out.print("Bitte eine Zahl oder -1 eingeben: ");
+			zahl = scan.nextInt();
+			scan.nextLine();
+			summe = summe +zahl;
+			
+		}while(zahl !=-1);
+		
+		System.out.println("Die Summe der eingegebenen Werte ist: " + summe);
+		
+		scan.close();
+	}
+
+}
